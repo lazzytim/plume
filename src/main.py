d05c538b80576ffe593d7eb28f3b3820f91fd81a
Timothy Deletrez <deletrez.timothy@gmail.com>
2024-09-18 15:03:48 +0200

    ⬆️ Upload of file

---
+++ src/main.py
@@ -0,0 +1,39 @@
+import customtkinter as ctk
+from PIL import Image, ImageTk
+import sys, pathlib, os
+
+def getPath(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = pathlib.Path(__file__).parent
+    return os.path.join(base_path, relative_path)
+
+root = ctk.CTk(fg_color='#181818')
+root.title('Plume for Windows')
+root.iconbitmap(getPath('icon.ico'))
+root.geometry('400x500')
+root.resizable(0, 0)
+
+ctk.CTkLabel(root, text='', image=ImageTk.PhotoImage(Image.open(getPath('banner.png')))).place(x=0, y=0, anchor=ctk.NW)
+
+tabview = ctk.CTkTabview(root, width=360, height=380, fg_color='#181818', segmented_button_selected_color='#ffd600', text_color='#181818', border_color='#434343', border_width=1)
+tabview_shortcutarrow = tabview.add('   Shortcuts Icon   ')
+tabview_taskbar = tabview.add('   Taskbar   ')
+tabview.place(x=20, y=100, anchor=ctk.NW)
+
+def shortcut_arrow(new : str):
+    os.mkdir('C:\\Plume')
+    os.rename(new, 'C:\\Plume\\shortcutarrow.ico')
+    file = open('regs\\temp.reg', mode='w')
+    file.write('Windows Registry Editor Version 5.00\n\n[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons]\n"29"="C:\\Plume\\shortcutarrow.ico"\n\n')
+    os.open('regs\\temp.reg')
+ 
+path = ctk.CTkEntry(tabview_shortcutarrow, fg_color='#181818', placeholder_text='Select a .ico to replace current one', border_width=1, width=245)
+path.place(x=20, y=20, anchor=ctk.NW)
+ctk.CTkButton(tabview_shortcutarrow, text="•••", width=50, fg_color='#ffd600', hover_color='#434343', text_color='#181818', command=getPath).place(x=280, y=20, anchor=ctk.NW)
+ctk.CTkLabel(tabview_shortcutarrow, text='', image=ImageTk.PhotoImage(Image.open(getPath('assets\\preview1.png')))).place(x=20, y=65, anchor=ctk.NW)
+ctk.CTkButton(tabview_shortcutarrow, text="Apply the modification", width=310, fg_color='#ffd600', hover_color='#434343', text_color='#181818', command=getPath).place(x=20, y=290, anchor=ctk.NW)
+
+ 
+root.mainloop()
---
+++ src/regs/README.txt
@@ -0,0 +1,5 @@
+DO NOT TOUCH THE FILES IN THIS FOLDER, 
+These are incomplete and can damage your computer if not executed with the software.
+
+To start the software, go back to the 
+previous folder, and start main.py.
