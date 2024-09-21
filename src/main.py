63e3727a75d9d2e838739b0d6e095a9f0adb2fbc
Timothy Deletrez <deletrez.timothy@gmail.com>
2024-09-21 10:41:55 +0200

    🔁 Updated functions
    - Rewrited functions to open file explorer (filedialog)
    - Linked the functions to their button
    - Added a .replace line to the "shortcut_arrow_apply()" function.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 import sys, pathlib, os
+import tkinter.filedialog as rf
 
 def getPath(relative_path):
     try:
@@ -22,18 +23,24 @@ tabview_shortcutarrow = tabview.add('   Shortcuts Icon   ')
 tabview_taskbar = tabview.add('   Taskbar   ')
 tabview.place(x=20, y=100, anchor=ctk.NW)
 
-def shortcut_arrow(new : str):
-    os.mkdir('C:\\Plume')
-    os.rename(new, 'C:\\Plume\\shortcutarrow.ico')
-    file = open('regs\\temp.reg', mode='w')
-    file.write('Windows Registry Editor Version 5.00\n\n[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons]\n"29"="C:\\Plume\\shortcutarrow.ico"\n\n')
-    os.open('regs\\temp.reg')
- 
 path = ctk.CTkEntry(tabview_shortcutarrow, fg_color='#181818', placeholder_text='Select a .ico to replace current one', border_width=1, width=245)
 path.place(x=20, y=20, anchor=ctk.NW)
-ctk.CTkButton(tabview_shortcutarrow, text="•••", width=50, fg_color='#ffd600', hover_color='#434343', text_color='#181818', command=getPath).place(x=280, y=20, anchor=ctk.NW)
+
+def shortcut_arrow_apply():
+    os.mkdir('C:\\Plume')
+    newpath = path.get().replace('/', '\\\\')
+    os.rename(newpath, 'C:\\Plume\\shortcutarrow.ico')
+    file = open('regs\\temp.reg', mode='w')
+    file.write('Windows Registry Editor Version 5.00\n\n[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Icons]\n"29"="C:\\Plume\\shortcutarrow.ico"\n\n')
+    os.open('regs\\temp.reg')
+    
+def shortcut_arrow_input():
+    file = rf.askopenfile(title='Select a .ICO format to change the shortcut arrow with.', filetypes=[("Windows Icon File","*.ico")]).name
+    path.insert(0, file)
+
+ctk.CTkButton(tabview_shortcutarrow, text="•••", width=50, fg_color='#ffd600', hover_color='#434343', text_color='#181818', command=shortcut_arrow_input).place(x=280, y=20, anchor=ctk.NW)
 ctk.CTkLabel(tabview_shortcutarrow, text='', image=ImageTk.PhotoImage(Image.open(getPath('assets\\preview1.png')))).place(x=20, y=65, anchor=ctk.NW)
-ctk.CTkButton(tabview_shortcutarrow, text="Apply the modification", width=310, fg_color='#ffd600', hover_color='#434343', text_color='#181818', command=getPath).place(x=20, y=290, anchor=ctk.NW)
+ctk.CTkButton(tabview_shortcutarrow, text="Apply the modification", width=310, fg_color='#ffd600', hover_color='#434343', text_color='#181818', command=shortcut_arrow_apply).place(x=20, y=290, anchor=ctk.NW)
 
  
 root.mainloop()
